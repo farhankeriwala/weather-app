8fd2279f60ad586d57aeda0947b72a0ab002afd5
Farhan Keriwala <114573891+farhankeriwala@users.noreply.github.com>
2023-12-18 18:00:33 +0000

    Add files via upload

---
+++ src/components/Search.js
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+import { AsyncPaginate } from "react-select-async-paginate";
+const Search = ({ onSearchChange }) => {
+  const [search, setSearch] = useState(null);
+
+  const loadOptions = async (inputValue) => {
+    const URL = `https://wft-geo-db.p.rapidapi.com/v1/geo/cities?minPopulation=1000000&namePrefix=${inputValue}`;
+    const options = {
+      method: "GET",
+      headers: {
+        "X-RapidAPI-Key": "f1c816f37cmsh22deae249ee8ccdp19b2e0jsn82cd4f9781c5",
+        "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+      },
+    };
+
+    try {
+      return fetch(URL, options)
+        .then((response) => response.json())
+        .then((response) => {
+          return {
+            options: response.data.map((searchCity) => {
+              return {
+                value: `${searchCity.latitude} ${searchCity.longitude}`,
+                label: `${searchCity.name}, ${searchCity.countryCode}`,
+              };
+            }),
+          };
+        });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleOnChange = (searchData) => {
+    setSearch(searchData);
+    onSearchChange(searchData);
+  };
+
+  return (
+    <AsyncPaginate
+      placeholder="Search for city"
+      debounceTimeout={500}
+      value={search}
+      onChange={handleOnChange}
+      loadOptions={loadOptions}
+    />
+  );
+};
+
+export default Search;
